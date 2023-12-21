fa1b62a725be8ee19bffefe6d98b100c3d7647cb
Rahulk1707 <rahulkabugade17@gmail.com>
2023-12-21 11:10:04 +0530

    Nothing changed...

--- src/test/java/com/eNetBanking/testCases/TC_LoginDDT_002.java
+++ src/test/java/com/eNetBanking/testCases/TC_LoginDDT_002.java
@@ -10,8 +10,8 @@ import org.testng.annotations.Test;
 import com.eNetBanking.pageObjects.LoginPage;
 import com.eNetBanking.utilities.XLUtils;
 
-public class TC_LoginDDT_002 extends BaseClass{
-	
+public class TC_LoginDDT_002 extends BaseClass {
+
 	@Test(dataProvider = "LoginData")
 	public void loginDDT(String user, String pwd) throws InterruptedException {
 		LoginPage lp = new LoginPage(driver);
@@ -21,8 +21,8 @@ public class TC_LoginDDT_002 extends BaseClass{
 		logger.info("Password Entered");
 		lp.clickSubmit();
 		Thread.sleep(3000);
-		
-		if(isAlertPresent() == true) {
+
+		if (isAlertPresent() == true) {
 			driver.switchTo().alert().accept();
 			driver.switchTo().defaultContent();
 			Assert.assertTrue(false);
@@ -36,32 +36,33 @@ public class TC_LoginDDT_002 extends BaseClass{
 			driver.switchTo().defaultContent();
 		}
 	}
-	
+
 	public boolean isAlertPresent() {
 		try {
 			driver.switchTo().alert().accept();
 			return true;
-		} catch(NoAlertPresentException e) {
+		} catch (NoAlertPresentException e) {
 			return false;
 		}
 	}
-	
+
 	@DataProvider(name = "LoginData")
 	String[][] getData() throws IOException {
 		String path = System.getProperty("user.dir") + "/src/test/java/com/eNetBanking/testData/LoginData.xlsx";
-		
+
 		int rowcount = XLUtils.getRowCount(path, "Sheet1");
 		int colcount = XLUtils.getCellCount(path, "Sheet1", 1);
-		
+
 		String logindata[][] = new String[rowcount][colcount];
-		
-		for(int i = 1; i <= rowcount; i++) {
-			
-			for(int j = 0; j < colcount; j++) {
+
+		for (int i = 1; i <= rowcount; i++) {
+
+			for (int j = 0; j < colcount; j++) {
 				logindata[i - 1][j] = XLUtils.getCellData(path, "Sheet1", i, j);
 			}
-			
+
 		}
+
 		return logindata;
 	}
 
